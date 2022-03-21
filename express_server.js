9fdf09afa1a5135e5edb1b815c4c47730009a6f1
Kurt Maglalang <kbm126@live.com>
2022-03-21 22:45:04 +0000

    setup partials with ejs

--- express_server.js
+++ express_server.js
@@ -4,20 +4,26 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.set('view enginer', 'ejs');
-
 
 app.get('/', (req, res) => {
   res.send("Hello");
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: '' };
+  res.render("hello_world", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  // res.send('url');
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
