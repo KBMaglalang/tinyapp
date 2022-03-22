e90959de433ce931b639eedb6792da919a641536
Kurt Maglalang <kbm126@live.com>
2022-03-22 05:53:34 +0000

    redirect the user from the shortURL to the long URL

--- express_server.js
+++ express_server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com/"
 };
 
 
@@ -31,33 +31,40 @@ app.get('/', (req, res) => {
   res.send("Hello");
 });
 
-// app.get("/hello", (req, res) => {
-//   const templateVars = { greeting: '' };
-//   res.render("hello_world", templateVars);
-// });
-
-app.get("/urls", (req, res) => {
-  // res.send('url');
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send("ok");
-});
-
+// return the urls available in the database
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// create a new shortened url web form
 app.get('/urls/new', (req,res) => {
   res.render('urls_new');
 });
 
+// show the list of urls that is stored
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+// add new url to the database
+app.post('/urls', (req, res) => {
+  let newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;
+  // res.send("ok");
+  res.redirect(`/urls/${newShortURL}`);
+});
+
+// showing the user their newly created link
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+// redirect to the new web form
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
