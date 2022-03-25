9d2c434e7a1f113be85cbff55d00937308b834b8
Kurt Maglalang <kbm126@live.com>
2022-03-25 01:44:56 +0000

    refactoring and commenting

--- express_server.js
+++ express_server.js
@@ -1,12 +1,21 @@
 // express_server.js
 
+// modules used in project
 const express = require('express');
 const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
-
+const morgan = require('morgan');
 const PORT = 8080;
 const app = express();
+
+/*****************************
+ *
+ *         MIDDLEWARE
+ *
+******************************/
+
+app.use(morgan('dev'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
@@ -16,7 +25,11 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
-
+/*****************************
+ *
+ *    DATABASE INFORMATION
+ *
+******************************/
 
 const urlDatabase = {
   b6UTxQ: {
@@ -46,11 +59,16 @@ const users = {
   }
 };
 
+/*****************************
+ *
+ *       HELPER FUNCTIONS
+ *
+******************************/
 
-
+// Gather URLs that belongs to a user
 const urlsForUser = function(id) {
-  let userURL = {};
-  for (let key in urlDatabase) {
+  const userURL = {};
+  for (const key in urlDatabase) {
     if (urlDatabase[key].userID === id) {
       userURL[key] = urlDatabase[key];
     }
@@ -58,6 +76,7 @@ const urlsForUser = function(id) {
   return userURL;
 };
 
+// Check if a user is exists in the database from their email
 const isUserEmailInDatabase = function(userEmail) {
   for (const key in users) {
     if (users[key].email === userEmail) {
@@ -67,18 +86,18 @@ const isUserEmailInDatabase = function(userEmail) {
   return false;
 };
 
+//  Generate a string of 6 random characters
 const generateRandomString = function() {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
+  return Math.random().toString(32).substring(2,8);
 };
 
+/*****************************
+ *
+ *        SERVER ROUTES
+ *
+******************************/
 
-
-// root page
+// root route
 app.get('/', (req, res) => {
   if (req.session.user_id) {
     res.redirect('/urls');
@@ -87,74 +106,76 @@ app.get('/', (req, res) => {
   res.redirect('/login');
 });
 
-// user logout page
+// logout the user
 app.post('/logout', (req, res) => {
   req.session = null;
   res.redirect('/urls');
 });
 
-// user login page
+// show user login page
 app.get("/login", (req, res) => {
+  const user = users[req.session.user_id];
   if (req.session.user_id) {
     res.redirect('/urls');
     return;
   }
-  const templateVars = { user: users[req.session.user_id] };
+  const templateVars = { user };
   res.render("urls_login", templateVars);
 });
 
-// user post login route
+// login an existing user to the serveice
 app.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
   // check if the user doesn't exist in the datbase
-  if (!req.body.email || !req.body.password) {
-    return res.status(400).send(`Cannot Leave Email and Password Empty`);
+  if (!email || !password) {
+    return res.status(400).send(`Cannot Leave Email and Password Empty. Please <a href="/login">Login</a>`);
   }
   
-  let userData = isUserEmailInDatabase(req.body.email);
+  const userData = isUserEmailInDatabase(email);
   if (userData === false) {
-    return res.status(403).send(`User Does Not Exist in Database`);
+    return res.status(400).send(`The username or password is incorrect. Please <a href="/login">Login</a>`);
   }
 
   // check if the password matches with the one stored in the datbase
-  if (!bcrypt.compareSync(req.body.password,userData.password)) {
-    return res.status(403).send(`Incorrect Password`);
-  }
-  
-  //set the appropriate cookie
-  req.session.user_id = userData.id;
-  res.redirect('/urls');
+  bcrypt.compare(password, userData.password, (err, result) => {
+    if (!result) {
+      return res.status(400).send(`The username or password is incorrect. Please <a href="/login">Login</a>`);
+    }
+    req.session.user_id = userData.id;
+    res.redirect('/urls');
+  });
 });
 
-// user registration page
+// show user registration page
 app.get('/register', (req,res) => {
   if (req.session.user_id) {
     res.redirect('/urls');
     return;
   }
-  const templateVars = {user: users[req.session.user_id]};
+  const user = users[req.session.user_id];
+  const templateVars = { user };
   res.render('urls_register', templateVars);
 });
 
-// post user registration route
+// register new user into user database
 app.post('/register', (req,res)=>{
   // bad input check
   if (!req.body.email || !req.body.password) {
-    return res.status(400).send(`Cannot Leave Email and Password Empty`);
+    return res.status(400).send(`Cannot Leave Email and Password Empty. Please <a href="/register">Register</a>`);
   }
-  
   // user already exists in datbase
   if (isUserEmailInDatabase(req.body.email) !== false) {
-    return res.status(400).send(`User Already Exists in Database`);
+    return res.status(400).send(`User Already Exists in Database. Please <a href="/register">Register</a>`);
   }
-  
   // setup new user
-  let newUserRandomID = generateRandomString();
+  const newUserRandomID = generateRandomString();
   users[newUserRandomID] = {
     id: newUserRandomID,
     email: req.body.email,
     password: bcrypt.hashSync(req.body.password, 10)
   };
-
   req.session.user_id = newUserRandomID;
   res.redirect('/urls');
 });
@@ -166,98 +187,96 @@ app.get('/urls/new', (req,res) => {
     return;
   }
   
-  const templateVars = {user: users[req.session.user_id]};
+  const user = users[req.session.user_id];
+  const templateVars = { user };
   res.render('urls_new', templateVars);
 });
 
 // show the list of urls that is stored
 app.get("/urls", (req, res) => {
   if (!req.session.user_id) {
-    res.status(401).send('Please Login or Register');
+    res.status(400).send('Please <a href="/login">Login</a> or <a href="/register">Register</a>');
     return;
   }
-
-  let usersURL = urlsForUser(req.session.user_id);
-  const templateVars = { urls: usersURL, user: users[req.session.user_id] };
+  const user = users[req.session.user_id];
+  const urls = urlsForUser(req.session.user_id);
+  const templateVars = { urls, user };
   res.render("urls_index", templateVars);
 });
 
 // add new url to the database
 app.post('/urls', (req, res) => {
-  if (!req.session.user_id) {
-    return res.status(403).send('Unable to create shortURLs. Please Login');
+  const longURL = req.body.longURL;
+  const userID = req.session.user_id;
+  if (!userID) {
+    return res.status(400).send('Unable to create shortURLs. Please <a href="/login">Login</a>');
   }
-  
-  if (!req.body.longURL) {
-    return res.status(400).send('Invalid URL');
+  if (!longURL) {
+    return res.status(406).send('Invalid URL');
   }
-
-  let newShortURL = generateRandomString();
-  urlDatabase[newShortURL] = {
-    longURL: req.body.longURL,
-    userID: req.session.user_id
-  };
+  const newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = { longURL , userID };
   res.redirect(`/urls/${newShortURL}`);
 });
 
 // delete the url
 app.post('/urls/:shortURL/delete', (req, res)=> {
+  const shortURL = req.params.shortURL;
   if (!req.session.user_id) {
-    return res.status(403).send('Unable to Delete shortURLs');
+    return res.status(400).send('Unable to Delete shortURLs. Please <a href="/login">Login</a>');
   }
-  if (urlDatabase[req.params.shortURL].userID !== req.session.user_id) {
+  if (urlDatabase[shortURL].userID !== req.session.user_id) {
     return res.status(403).send('Unable to Delete Another Users shortURLs');
   }
-  delete urlDatabase[req.params.shortURL];
+  delete urlDatabase[shortURL];
   res.redirect('/urls');
 });
 
 // edit the url
 app.post('/urls/:shortURL', (req, res)=> {
+  const shortURL = req.params.shortURL;
   if (!req.session.user_id) {
-    return res.status(403).send('Unable to Edit shortURLs');
+    return res.status(400).send('Unable to Edit shortURLs. Please <a href="/login">Login</a>');
   }
-  if (urlDatabase[req.params.shortURL].userID !== req.session.user_id) {
+  if (urlDatabase[shortURL].userID !== req.session.user_id) {
     return res.status(403).send('Unable to Edit Another Users shortURLs');
   }
-  urlDatabase[req.params.shortURL].longURL = req.body.longURL;
-  // res.redirect(`/urls/${req.params.shortURL}`);
+  urlDatabase[shortURL].longURL = req.body.longURL;
   res.redirect(`/urls`);
 });
 
 // showing the user their newly created link
 app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  
   if (!req.session.user_id) {
-    return res.status(403).send('Please Login');
+    return res.status(400).send('Please <a href="/login">Login</a>');
   }
-  if (!urlDatabase[req.params.shortURL]) {
-    return res.status(404).send(`Provided shortURL [${req.params.shortURL}] not found in database`);
+  if (!urlDatabase[shortURL]) {
+    return res.status(404).send(`Provided shortURL [${shortURL}] not found in database`);
   }
-  if (urlDatabase[req.params.shortURL].userID !== req.session.user_id) {
-    return res.status(403).send('Forbidden');
+  if (urlDatabase[shortURL].userID !== req.session.user_id) {
+    return res.status(403).send('Unable to See Another Users shortURLs');
   }
-  const templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL].longURL,
-    user: users[req.session.user_id]
-  };
+  const longURL = urlDatabase[shortURL].longURL;
+  const user = users[req.session.user_id];
+  const templateVars = {shortURL, longURL, user};
   res.render("urls_show", templateVars);
 });
 
 // redirect to the new web form
 app.get("/u/:shortURL", (req, res) => {
-  if (!urlDatabase[req.params.shortURL]) {
-    return res.status(404).send(`Provided shortURL [${req.params.shortURL}] not found in database`);
+  const shortURL = req.params.shortURL;
+  if (!urlDatabase[shortURL]) {
+    return res.status(404).send(`Provided shortURL [${shortURL}] not found in database`);
   }
-  const longURL = urlDatabase[req.params.shortURL].longURL;
+  const longURL = urlDatabase[shortURL].longURL;
   res.redirect(longURL);
 });
 
-// wild card route
-app.get("*", (req, res) => {
-  return res.status(404).send(`Not Found`);
-});
 
+
+// setup server to list on PORT
 app.listen(PORT, () => {
   console.log(`Tiny app listening on port ${PORT}`);
 });
